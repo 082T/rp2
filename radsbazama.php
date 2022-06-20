20d7faddfe2030e7c5dcd0451af8eac903adb7cb
rokoroks <43017115+rokoroks@users.noreply.github.com>
2022-06-21 00:42:47 +0200

    Add files via upload

---
+++ radsbazama.php
@@ -0,0 +1,18 @@
+<?php 
+    
+    $dbname = 'prezime';
+    $db = new PDO('mysql:host=rp2.studenti.math.hr;dbname=' . $dbname . ';charset=utf8', 'student', 'pass.-----');
+
+
+    $st = $db->query('SELECT JMBAG,Ime,Prezime FROM Studenti');
+
+    foreach($st->fetchAll() as $row)
+    {
+        echo "JMBAG = " . $row['JMBAG'];
+        echo " Ime = " . $row['Ime'];
+        echo " Prezime = " . $row['Prezime'];
+        echo "<br>";
+    }
+
+
+?> 
